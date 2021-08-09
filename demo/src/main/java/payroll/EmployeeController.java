5befe475059fe7b2dfa233e0d48a89b272ef2b5a
Haaji <soldier.mh.2001@gmail.com>
2021-08-09 11:48:16 +0430

    Order class added

--- demo/src/main/java/payroll/EmployeeController.java
+++ demo/src/main/java/payroll/EmployeeController.java
@@ -89,8 +89,11 @@ class EmployeeController {
     }
 
     @DeleteMapping("/employees/{id}")
-    void deleteEmployee(@PathVariable Long id) {
+    ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+
         repository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 
     //test
